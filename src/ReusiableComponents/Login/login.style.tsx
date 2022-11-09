2a6bcca0a1c096ea5a335645e7059f6940fe9111
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2022-11-09 17:26:57 +0500

    feat: responsive-login

--- src/ReusiableComponents/Login/login.style.tsx
+++ src/ReusiableComponents/Login/login.style.tsx
@@ -1,6 +1,7 @@
 import {makeStyles} from "@material-ui/core";
 import { styled } from '@mui/material/styles';
 import Button, { ButtonProps } from '@mui/material/Button';
+
 export default makeStyles((theme) => ({
     loginWrapper: {
         width: "100%",
@@ -19,15 +20,24 @@ export default makeStyles((theme) => ({
         [theme.breakpoints.down('sm')]: {
             fontSize: "24px",
             lineHeight: "25.2px",
-            padding: "50px 0 0 50px",
+            padding: "50px 0 0 30px",
         },
     },
     formBox: {
         padding: "200px 0 0 100px",
         maxWidth: "500px",
+        [theme.breakpoints.down('sm')]: {
+            padding: "150px 0 0 10px",
+            maxWidth: "350px",
+        },
     },
     formDiv: {
-        '& > :not(style)': { m: 1, width: '400px', background: "rgba(255, 255, 255, 0.8)",},
+        '& > :not(style)': {
+            m: 1, width: '400px', background: "rgba(255, 255, 255, 0.8)",
+            [theme.breakpoints.down('sm')]: {
+                maxWidth: "350px",
+            },
+        },
         marginTop: "50px",
         color: "#fff !important",
         '& input:valid + fieldset': {
@@ -45,11 +55,13 @@ export const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
     color: "#fff",
     backgroundColor: "#bf0000",
     marginTop: "50px",
-    width: "100%",
+    width: "400px",
     padding: "15px 0",
     '&:hover': {
         opacity: "0.9",
         backgroundColor: "#bf0000",
     },
+    [theme.breakpoints.down('sm')]: {
+        maxWidth: "350px",
+    },
 }));
-
